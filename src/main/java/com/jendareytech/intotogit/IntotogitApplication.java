a2ec66c22ed917438a2fb035182a08d149db00b0
nenycoc <naafn2004@gmail.com>
2024-01-28 15:01:13 -0600

    adding files

---
+++ src/main/java/com/jendareytech/intotogit/IntotogitApplication.java
@@ -0,0 +1,13 @@
+package com.jendareytech.intotogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntotogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntotogitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jendareytech/intotogit/IntotogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jendareytech.intotogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntotogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
